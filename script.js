5b838b0812d0059404cb926fc1ef1e269491d9d0
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-05-13 10:38:14 +0200

    add css modifiche

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ new Vue({
     el: "#app",
 
     data: {
-        albumList:[],
-        filteredData:[],
-        genre:null
+        albumList: [],
+        filteredData: [],
+        genre: null
     },
 
 
     methods: {
-        
+
         onSelectChange() {
 
-            this.filteredData  = [];
-            for (let index = 0; index < this.albumList.length; index++) {
-                const element = this.albumList[index];
+            this.filteredData = [];
+            for (let i = 0; i < this.albumList.length; i++) {
+                const element = this.albumList[i];
                 if (element.genre == this.genre) {
                     this.filteredData.push(element);
                 }
@@ -26,7 +26,7 @@ new Vue({
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then((resp) => {
-     
+
                 this.albumList = resp.data.response;
                 this.filteredData = this.albumList;
             });
